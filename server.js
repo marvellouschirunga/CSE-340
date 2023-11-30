d243a427eb617cdba1e7a4763736ed9fbb9a1d40
Marvellous Humphery Chirunga <marvellouschirunga@gmail.com>
2023-11-30 23:45:26 +0200

    fixed server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  if(err.status == 404){ message = err.message} else {message = 'Great Scott! There was a crash. Maybe try a different route?'}
+  if(err.status == 404){ message = err.message} else {message = 'Oops..Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
     message,
